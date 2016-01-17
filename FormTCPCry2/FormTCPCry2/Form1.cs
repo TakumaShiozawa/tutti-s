603081232048e50b307e00bf5991fcfa79c0c7fa
fragile <cycad1514@gmail.com>
2016-01-18 08:49:37 +0900

    TCP/IP C#　チャットプログラム　UP

---
+++ FormTCPCry2/FormTCPCry2/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FormTCPCry2
+{
+    public partial class Form1 : Form
+    {
+        System.Net.Sockets.TcpClient tcp;
+        System.Net.Sockets.NetworkStream ns;
+        bool socet;
+        public Form1()
+        {
+            InitializeComponent();
+            //垂直、水平両方のスクロールバーを表示"
+            textBox1.ScrollBars = ScrollBars.Both;
+            socet = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                //サーバーのIPアドレス（または、ホスト名）とポート番号
+                //string ipOrHost = "127.0.0.1";
+                //string ipOrHost = "127.0.0.1";
+                string ipOrHost = textBox3.Text;
+                //string ipOrHost = "localhost";
+                //int port = 2001;
+                int port = int.Parse(textBox4.Text);
+                //TcpClientを作成し、サーバーと接続する
+                tcp = new System.Net.Sockets.TcpClient(ipOrHost, port);
+                /*
+                Console.WriteLine("サーバー({0}:{1})と接続しました({2}:{3})。",
+                    ((System.Net.IPEndPoint)tcp.Client.RemoteEndPoint).Address,
+                    ((System.Net.IPEndPoint)tcp.Client.RemoteEndPoint).Port,
+                    ((System.Net.IPEndPoint)tcp.Client.LocalEndPoint).Address,
+                    ((System.Net.IPEndPoint)tcp.Client.LocalEndPoint).Port);
+                */
+                label1.Text = "サーバー(" + ((System.Net.IPEndPoint)tcp.Client.RemoteEndPoint).Address + ":" + ((System.Net.IPEndPoint)tcp.Client.RemoteEndPoint).Port + ")と接続しました("
+                    + ((System.Net.IPEndPoint)tcp.Client.LocalEndPoint).Address + ":" + ((System.Net.IPEndPoint)tcp.Client.LocalEndPoint).Port + ")。";
+                //NetworkStreamを取得する
+                ns = tcp.GetStream();
+
+                //読み取り、書き込みのタイムアウトを10秒にする
+                //デフォルトはInfiniteで、タイムアウトしない
+                //(.NET Framework 2.0以上が必要)
+                ns.ReadTimeout = 10000;
+                ns.WriteTimeout = 10000;
+                socet = true;
+            }
+            catch (System.Net.Sockets.SocketException ex)
+            {
+                MessageBox.Show(ex.ToString()+"\nサーバー側の準備ができていません", "エラー");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(ns!=null){
+                //閉じる
+                ns.Close();
+                tcp.Close();
+                Console.WriteLine("切断しました。");
+
+                //Console.ReadLine();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //サーバーに送信するデータを入力してもらう
+                //Console.WriteLine("文字列を入力し、Enterキーを押してください。");
+                //string sendMsg = Console.ReadLine();
+
+                //メッセージ取得
+                string sendMsg = textBox2.Text + "\r\n";
+
+                //何も入力されなかった時は終了
+                if (sendMsg == null || sendMsg.Length == 0)
+                {
+                    return;
+                }
+
+                //サーバーにデータを送信する
+                //文字列をByte型配列に変換
+                System.Text.Encoding enc = System.Text.Encoding.UTF8;
+                byte[] sendBytes = enc.GetBytes(sendMsg + '\n');
+                //データを送信する
+                ns.Write(sendBytes, 0, sendBytes.Length);
+                textBox1.Text += sendMsg;
+                Console.WriteLine(sendMsg);
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("接続されていません。","エラー");
+            }
+            
+        }
+
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (socet && tcp.Available > 0)
+            {
+                //サーバーから送られたデータを受信する
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                byte[] resBytes = new byte[256];
+                int resSize = 0;
+                do
+                {
+                    //データの一部を受信する
+                    resSize = ns.Read(resBytes, 0, resBytes.Length);
+                    //Readが0を返した時はサーバーが切断したと判断
+                    if (resSize == 0)
+                    {
+                        Console.WriteLine("サーバーが切断しました。");
+                        break;
+                    }
+                    //受信したデータを蓄積する
+                    ms.Write(resBytes, 0, resSize);
+                    //まだ読み取れるデータがあるか、データの最後が\nでない時は、
+                    // 受信を続ける
+                } while (ns.DataAvailable || resBytes[resSize - 1] != '\n');
+                //受信したデータを文字列に変換
+                System.Text.Encoding enc = System.Text.Encoding.UTF8;
+                string resMsg = enc.GetString(ms.GetBuffer(), 0, (int)ms.Length);
+                ms.Close();
+                //末尾の\nを削除
+                resMsg = resMsg.TrimEnd('\n');
+                Console.WriteLine(resMsg);
+                textBox1.Text += resMsg+"\r\n";
+            }
+        }
+    }
+}
---
+++ FormTCPSvr/FormTCPSvr/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FormTCPSvr
+{
+    public partial class Form1 : Form
+    {
+        System.Net.Sockets.TcpClient client;
+        System.Net.Sockets.TcpListener listener;
+        System.Net.Sockets.NetworkStream ns;
+        bool disconnected;
+        bool socet;
+        public Form1()
+        {
+            InitializeComponent();
+            socet = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ns == null)
+            {
+                //ListenするIPアドレス
+                //string ipString = "127.0.0.1";
+                string ipString = textBox3.Text;
+                System.Net.IPAddress ipAdd = System.Net.IPAddress.Parse(ipString);
+
+                //ホスト名からIPアドレスを取得する時は、次のようにする
+                //string host = "localhost";
+                //System.Net.IPAddress ipAdd =
+                //    System.Net.Dns.GetHostEntry(host).AddressList[0];
+                //.NET Framework 1.1以前では、以下のようにする
+                //System.Net.IPAddress ipAdd =
+                //    System.Net.Dns.Resolve(host).AddressList[0];
+
+                //Listenするポート番号
+                int port = int.Parse(textBox4.Text);
+
+                //TcpListenerオブジェクトを作成する
+                listener = new System.Net.Sockets.TcpListener(ipAdd, port);
+
+                //Listenを開始する
+                listener.Start();
+                label1.Text = "Listenを開始しました(" + ((System.Net.IPEndPoint)listener.LocalEndpoint).Address + ":" + ((System.Net.IPEndPoint)listener.LocalEndpoint).Port + ")。";
+
+                //接続要求があったら受け入れる
+                client = listener.AcceptTcpClient();
+                label1.Text = "クライアント(" + ((System.Net.IPEndPoint)client.Client.RemoteEndPoint).Address + ":" + ((System.Net.IPEndPoint)client.Client.RemoteEndPoint).Port + ")と接続しました。";
+
+                //NetworkStreamを取得
+                ns = client.GetStream();
+
+                //読み取り、書き込みのタイムアウトを10秒にする
+                //デフォルトはInfiniteで、タイムアウトしない
+                //(.NET Framework 2.0以上が必要)
+                ns.ReadTimeout = 10000;
+                ns.WriteTimeout = 10000;
+                socet = true;
+            }
+
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ns != null)
+            {
+                //閉じる
+                ns.Close();
+                client.Close();
+                Console.WriteLine("クライアントとの接続を閉じました。");
+
+                //リスナを閉じる
+                listener.Stop();
+                Console.WriteLine("Listenerを閉じました。");
+
+            }
+            //Console.ReadLine();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (!disconnected)
+                {
+                    //クライアントにデータを送信する
+                    //クライアントに送信する文字列を作成
+                    string sendMsg = textBox2.Text + "\r\n";
+                    //文字列をByte型配列に変換
+                    System.Text.Encoding enc = System.Text.Encoding.UTF8;
+                    byte[] sendBytes = enc.GetBytes(sendMsg + '\n');
+                    //データを送信する
+                    ns.Write(sendBytes, 0, sendBytes.Length);
+                    textBox1.Text += sendMsg;
+                    Console.WriteLine(sendMsg);
+                }
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("接続されていません。", "エラー");
+            }
+        }
+
+        //1秒ごとに受信
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (socet && client.Available > 0)
+            {
+                
+                try
+                {
+                    //クライアントから送られたデータを受信する
+                    
+                    disconnected = false;
+                    System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                    byte[] resBytes = new byte[256];
+                    int resSize = 0;
+                    do
+                    {
+                        //データの一部を受信する
+                        resSize = ns.Read(resBytes, 0, resBytes.Length);
+                        //Readが0を返した時はクライアントが切断したと判断
+                        if (resSize == 0)
+                        {
+                            disconnected = true;
+                            Console.WriteLine("クライアントが切断しました。");
+                            break;
+                        }
+                        //受信したデータを蓄積する
+                        ms.Write(resBytes, 0, resSize);
+                        //まだ読み取れるデータがあるか、データの最後が\nでない時は、
+                        // 受信を続ける
+                    } while (ns.DataAvailable || resBytes[resSize - 1] != '\n');
+                    //受信したデータを文字列に変換
+                    System.Text.Encoding enc = System.Text.Encoding.UTF8;
+                    string resMsg = enc.GetString(ms.GetBuffer(), 0, (int)ms.Length);
+                    ms.Close();
+                    //末尾の\nを削除
+                    resMsg = resMsg.TrimEnd('\n');
+                    //Console.WriteLine(resMsg);
+                    textBox1.Text +=resMsg+"\r\n";
+                }
+                catch (ObjectDisposedException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+
+       
+       
+    }
+}
