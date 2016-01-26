59223106e0adbf3ee1f42fd1a87e68f39a9c2ada
fragile <cycad1514@gmail.com>
2016-01-26 14:03:33 +0900

    image変更

--- DigitalSignage_Ver3_TCP/DigitalSignage_Ver3/obj/Debug/SkeletonBasics-WPF_Content.g.i.cs
+++ DigitalSignage_Ver3_TCP/DigitalSignage_Ver3/obj/Debug/SkeletonBasics-WPF_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/map.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/klspace3.png")]
 
 
